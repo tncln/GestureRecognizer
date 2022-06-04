0501a4e783e07daea3df558d2e9709a632dd87e0
Adem Tunçalın <tuncalin@outlook.com>
2022-06-04 22:38:54 +0300

    Jest işlemleri

--- GestureRecognizer/ViewController.swift
+++ GestureRecognizer/ViewController.swift
@@ -11,12 +11,29 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    var isJames = true
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //test
+        imageView.isUserInteractionEnabled = true //resmin tıklanabilir olup olmamasını sağlar
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changePic))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
-
+    @objc func changePic(){
+       
+        
+        if isJames == true {
+            imageView.image = UIImage(named: "lars")
+            myLabel.text = "Lars Ulrich"
+            isJames = false
+        } else {
+            imageView.image = UIImage(named: "james")
+            myLabel.text = "James Hetfield"
+            isJames = true
+            
+        }
+    }
 
 }
 
